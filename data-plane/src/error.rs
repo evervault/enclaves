293d1908cdc94ffb75dac5bfcac74e128f7c2c87
Chinaza Uzoukwu <chinaza@evervault.com>
2022-06-29 12:48:18 +0100

    Added error.rs

---
+++ data-plane/src/error.rs
@@ -0,0 +1,19 @@
+use std::fmt;
+
+pub enum Error {
+    Crypto(String)
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+	write!(f, "{}", match self {
+	    Error::Crypto(message) => message
+	})
+    }
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+	write!(f, "{}", self)
+    }
+}
