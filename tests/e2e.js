f06ac6750a53e1ef1d2467cc940e46be60600342
hanneary <92307259+hanneary@users.noreply.github.com>
2022-07-12 11:34:02 +0100

    Stop runit continuously running services (#21)

--- tests/e2e.js
+++ tests/e2e.js
@@ -3,7 +3,7 @@ const axios = require('axios').default;
 
 describe('POST data to enclave', () => {
     it('enclave responds and echos back body', async () => {
-        const result = await axios.post('http://127.0.0.1:3030/hello', { secret: 'ev:123' })
+        const result = await axios.post('http://localhost:3030/hello', { secret: 'ev:123' })
         expect(result.data).to.deep.equal({ response: 'Hello from enclave',  secret: 'ev:123'});
  });
 });
--- tests/scripts/start-ev-services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sv start data-plane
-while !(sv check data-plane); do sleep 1; done;
-sv start control-plane
-while !(sv check control-plane); do sleep 1; done;
