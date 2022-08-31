3bf028bd9f51b4bd9b920192c2a6e4f1da2bfe6d
Donal Tuohy <donaltuohy@gmail.com>
2022-08-31 14:38:57 +0100

    [SCL-449] Set EC2 instance ID in control plane container (#86)
    * [SCL-449] Set EC2 instance ID in control plane container
    
    * [SCL-449] Do it without AWS CLI

--- scripts/start-cage.sh
+++ scripts/start-cage.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+yum install wget
+INSTANCE_ID=$(wget -q -O - http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r ."instanceId")
+
+export EC2_INSTANCE_ID=${INSTANCE_ID}
 
 # Kill all existing enclaves on this host
 echo "[HOST] Deployment started. Terminating old enclave..."
