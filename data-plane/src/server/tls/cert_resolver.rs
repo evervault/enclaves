3f81efadf4af107995093c77656691a835b46670
Hannah Neary <92307259+hanneary@users.noreply.github.com>
2023-02-10 11:01:34 +0000

    Move unused ca generation code to test (#191)

--- data-plane/src/server/tls/cert_resolver.rs
+++ data-plane/src/server/tls/cert_resolver.rs
@@ -392,7 +392,6 @@ impl ResolvesServerCert for AttestableCertResolver {
 
 #[cfg(test)]
 mod tests {
-    use super::super::provisioner;
     use super::*;
     use nom::AsBytes;
     use openssl::x509::{X509Ref, X509};
@@ -538,7 +537,7 @@ mod tests {
             trx_logging,
         );
         let server_name = Some(ctx.get_cert_name());
-        let (cert, key) = provisioner::generate_ca().unwrap();
+        let (cert, key) = generate_ca().unwrap();
         let resolver = AttestableCertResolver::new(cert, key).unwrap();
         let first_cert = resolver
             .resolve_cert_using_sni(server_name.as_deref())
@@ -579,7 +578,7 @@ mod tests {
         );
         CageContext::set(ctx.clone()).unwrap();
         let server_name = Some(ctx.get_cert_name());
-        let (cert, key) = provisioner::generate_ca().unwrap();
+        let (cert, key) = generate_ca().unwrap();
         let resolver = AttestableCertResolver::new(cert, key).unwrap();
         let first_cert = resolver
             .resolve_cert_using_sni(server_name.as_deref())
@@ -624,7 +623,7 @@ mod tests {
         );
         let hostname = ctx.get_cert_name();
         let server_name = Some(hostname.clone());
-        let (cert, key) = provisioner::generate_ca().unwrap();
+        let (cert, key) = generate_ca().unwrap();
         let resolver = AttestableCertResolver::new(cert, key).unwrap();
         let first_cert = resolver
             .resolve_cert_using_sni(server_name.as_deref())
@@ -658,7 +657,7 @@ mod tests {
         );
         let hostname = ctx.get_cert_name();
         let server_name = Some(hostname.clone());
-        let (cert, key) = provisioner::generate_ca().unwrap();
+        let (cert, key) = generate_ca().unwrap();
         let resolver = AttestableCertResolver::new(cert, key).unwrap();
         let first_cert = resolver
             .resolve_cert_using_sni(server_name.as_deref())
@@ -673,5 +672,52 @@ mod tests {
             entry_string.to_string() == hostname
         });
         assert!(final_name_entry.is_some());
+    }
+
+    pub fn generate_ca() -> Result<(X509, PKey<Private>), ErrorStack> {
+        let ec_group = EcGroup::from_curve_name(Nid::SECP384R1)?;
+        let ec_key = EcKey::generate(ec_group.as_ref())?;
+        let key_pair = PKey::from_ec_key(ec_key)?;
+
+        let mut x509_name = X509NameBuilder::new()?;
+        x509_name.append_entry_by_text("C", "IE")?;
+        x509_name.append_entry_by_text("ST", "DUB")?;
+        x509_name.append_entry_by_text("O", "Evervault")?;
+        x509_name.append_entry_by_text("CN", "Data Plane Self Signed Cert")?;
+        let x509_name = x509_name.build();
+
+        let mut cert_builder = X509::builder()?;
+        cert_builder.set_version(2)?;
+        let serial_number = {
+            let mut serial = BigNum::new()?;
+            serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
+            serial.to_asn1_integer()?
+        };
+
+        cert_builder.set_serial_number(&serial_number)?;
+        cert_builder.set_subject_name(&x509_name)?;
+        cert_builder.set_issuer_name(&x509_name)?;
+        cert_builder.set_pubkey(&key_pair)?;
+        let not_before = Asn1Time::days_from_now(0)?;
+        cert_builder.set_not_before(&not_before)?;
+        let not_after = Asn1Time::days_from_now(365)?;
+        cert_builder.set_not_after(&not_after)?;
+
+        cert_builder.append_extension(BasicConstraints::new().critical().ca().build()?)?;
+        cert_builder.append_extension(
+            KeyUsage::new()
+                .critical()
+                .key_cert_sign()
+                .crl_sign()
+                .build()?,
+        )?;
+
+        let subject_key_identifier =
+            SubjectKeyIdentifier::new().build(&cert_builder.x509v3_context(None, None))?;
+        cert_builder.append_extension(subject_key_identifier)?;
+
+        cert_builder.sign(&key_pair, MessageDigest::sha256())?;
+        let cert = cert_builder.build();
+        Ok((cert, key_pair))
     }
 }
