713ce136fee290e9a98559fd83da063fb509b718
Hannah Neary <hannah@evervault.com>
2023-04-03 17:41:30 +0100

    Add backward compatibility to allowlist

--- data-plane/src/configuration.rs
+++ data-plane/src/configuration.rs
@@ -31,21 +31,22 @@ pub fn get_egress_ports() -> Vec<u16> {
 
 pub fn get_egress_allow_list() -> EgressDomains {
     let domain_str = std::env::var("EGRESS_ALLOW_LIST").unwrap_or("".to_string());
-    let domains: Vec<String> = domain_str
+    let (wildcard, exact): (Vec<String>, Vec<String>) = domain_str
         .split(',')
         .map(|domain| domain.to_string())
-        .collect();
-    let (wildcard, exact): (Vec<String>, Vec<String>) = domains
-        .clone()
-        .into_iter()
         .partition(|domain| domain.starts_with("*."));
     let wildcard_stripped = wildcard
         .iter()
-        .filter_map(|w| w.strip_prefix("*.").map(|domain| domain.to_string()))
+        .filter_map(|wc| wc.strip_prefix('*').map(|domain| domain.to_string()))
         .collect();
+    let exact_backwards_compat = if exact == vec![""] {
+        vec!["*".to_string()]
+    } else {
+        exact
+    };
     EgressDomains {
         wildcard: wildcard_stripped,
-        exact,
+        exact: exact_backwards_compat,
     }
 }
 
@@ -57,19 +58,44 @@ pub struct EgressDomains {
 
 #[cfg(test)]
 mod tests {
-    use crate::{configuration::{get_egress_allow_list, EgressDomains}};
+    use crate::configuration::{get_egress_allow_list, EgressDomains};
 
     #[test]
     fn test_valid_all_domains() {
         std::env::set_var("EGRESS_ALLOW_LIST", "*");
         let egress = get_egress_allow_list();
-        assert_eq!(egress, EgressDomains { exact: vec!["*".to_string()], wildcard: vec![]})
+        assert_eq!(
+            egress,
+            EgressDomains {
+                exact: vec!["*".to_string()],
+                wildcard: vec![]
+            }
+        )
     }
 
     #[test]
     fn test_wildcard_and_exact() {
         std::env::set_var("EGRESS_ALLOW_LIST", "*.evervault.com,google.com");
         let egress = get_egress_allow_list();
-        assert_eq!(egress, EgressDomains { exact: vec!["google.com".to_string()], wildcard: vec!["evervault.com".to_string()]})
+        assert_eq!(
+            egress,
+            EgressDomains {
+                exact: vec!["google.com".to_string()],
+                wildcard: vec![".evervault.com".to_string()]
+            }
+        )
+    }
+
+    #[test]
+    fn test_backwards_compat() {
+        std::env::set_var("EGRESS_ALLOW_LIST", "");
+        let egress = get_egress_allow_list();
+        assert_eq!(
+            egress,
+            EgressDomains {
+                exact: vec!["*".to_string()],
+                wildcard: vec![]
+            }
+        )
     }
 }
