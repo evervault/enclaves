8a5f906c3086f9e67c0b191fff041c3f9a5a0174
Hannah Neary <hannah@evervault.com>
2023-04-05 11:14:08 +0100

    Remove unused metrics

--- data-plane/src/stats_client.rs
+++ data-plane/src/stats_client.rs
@@ -89,12 +89,8 @@ impl StatsClient {
         if let Ok(context) = CageContext::get() {
             publish_gauge!("memory.total", mem_info.total as f64, context);
             publish_gauge!("memory.avail", mem_info.avail as f64, context);
-            publish_gauge!("memory.free", mem_info.total as f64, context);
-
             publish_gauge!("cpu.cores", cpu_num as f64, context);
             publish_gauge!("cpu.one", cpu.one, context);
-            publish_gauge!("cpu.five", cpu.five, context);
-            publish_gauge!("cpu.fifteen", cpu.fifteen, context);
         };
         Ok(())
     }
