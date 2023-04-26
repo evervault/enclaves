877299792bf6e16bef924ea025fae173a002e186
Liam <liam@evervault.com>
2023-04-26 12:28:14 +0100

    qualify paths of certs

--- e2e-tests/run-tls-disabled-tests.sh
+++ e2e-tests/run-tls-disabled-tests.sh
@@ -12,19 +12,17 @@ else
   # if in CI, generate certs and export them
   . e2e-tests/mtls-testing-certs/ca/generate-certs.sh
 
-  cd e2e-tests/mtls-testing-certs/ca/certs
-  MOCK_CRYPTO_CERT=`cat ca.crt` && export MOCK_CRYPTO_CERT
-  MOCK_CRYPTO_KEY=`cat ca.key` && export MOCK_CRYPTO_KEY
+  MOCK_CRYPTO_CERT=`cat e2e-tests/mtls-testing-certs/ca/certs/ca.crt` && export MOCK_CRYPTO_CERT
+  MOCK_CRYPTO_KEY=`cat e2e-tests/mtls-testing-certs/ca/certs/ca.key` && export MOCK_CRYPTO_KEY
 
 
-  MOCK_CERT_PROVISIONER_CLIENT_CERT=`cat client_0.crt` && export MOCK_CERT_PROVISIONER_CLIENT_CERT
-  MOCK_CERT_PROVISIONER_CLIENT_KEY=`cat client_0.key` && export MOCK_CERT_PROVISIONER_CLIENT_KEY
-  MOCK_CERT_PROVISIONER_ROOT_CERT=`cat ca.crt` && export MOCK_CERT_PROVISIONER_ROOT_CERT
+  MOCK_CERT_PROVISIONER_CLIENT_CERT=`cat e2e-tests/mtls-testing-certs/ca/certs/client_0.crt` && export MOCK_CERT_PROVISIONER_CLIENT_CERT
+  MOCK_CERT_PROVISIONER_CLIENT_KEY=`cat e2e-tests/mtls-testing-certs/ca/certs/client_0.key` && export MOCK_CERT_PROVISIONER_CLIENT_KEY
+  MOCK_CERT_PROVISIONER_ROOT_CERT=`cat e2e-tests/mtls-testing-certs/ca/certs/ca.crt` && export MOCK_CERT_PROVISIONER_ROOT_CERT
 
-  MOCK_CERT_PROVISIONER_SERVER_KEY=`cat localhost.key` && export MOCK_CERT_PROVISIONER_SERVER_KEY
-  MOCK_CERT_PROVISIONER_ROOT_CERT=`cat ca.crt` && export MOCK_CERT_PROVISIONER_ROOT_CERT
-  MOCK_CERT_PROVISIONER_SERVER_CERT=`cat localhost.crt` && export MOCK_CERT_PROVISIONER_SERVER_CERT
-  cd ../../../..
+  MOCK_CERT_PROVISIONER_SERVER_KEY=`cat e2e-tests/mtls-testing-certs/ca/certs/localhost.key` && export MOCK_CERT_PROVISIONER_SERVER_KEY
+  MOCK_CERT_PROVISIONER_ROOT_CERT=`cat e2e-tests/mtls-testing-certs/ca/certs/ca.crt` && export MOCK_CERT_PROVISIONER_ROOT_CERT
+  MOCK_CERT_PROVISIONER_SERVER_CERT=`cat e2e-tests/mtls-testing-certs/ca/certs/localhost.crt` && export MOCK_CERT_PROVISIONER_SERVER_CERT
 fi
 
 # install the node modules for customer process and test script
