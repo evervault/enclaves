7fa5c606644cc3cfbc0d57a29c6d1f4453024638
Liam <liam@evervault.com>
2023-07-03 14:34:17 +0100

    Add call to get metadata token before calling for instance id

--- scripts/start-cage.sh
+++ scripts/start-cage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-INSTANCE_ID=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r ."instanceId")
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60" -sL`
+INSTANCE_ID=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document -H "X-aws-ec2-metadata-token: $TOKEN" | jq -r ."instanceId")
 
 export EC2_INSTANCE_ID=${INSTANCE_ID}
 
