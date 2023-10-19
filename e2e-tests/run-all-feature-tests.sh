92d15934fc395a87402b1819d1138a461019755c
Liam <liam@evervault.com>
2023-10-19 10:49:47 +0100

    chore: update e2e tests to generate cert chain on the fly

--- e2e-tests/run-all-feature-tests.sh
+++ e2e-tests/run-all-feature-tests.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e 
 
+# If sample-ca directory doesn't exist, create it
+if [ ! -d "e2e-tests/sample-ca" ]; then
+  mkdir -p e2e-tests/sample-ca
+fi
+# Generate self signed root CA
+openssl genrsa -out e2e-tests/sample-ca/sample-root-ca-key.pem 2048
+openssl req -x509 -extensions v3_ca -subj "/C=US/ST=CA/L=San Francisco/O=Evervault/OU=Cages/CN=Cage Test Root CA" -sha256 -new -nodes -key e2e-tests/sample-ca/sample-root-ca-key.pem -days 3650 -out e2e-tests/sample-ca/sample-root-ca-cert.pem
+# Generate intermediate CA
+openssl genrsa -out e2e-tests/sample-ca/sample-intermediate-key.pem 2048
+openssl req -new -subj "/C=US/ST=CA/L=San Francisco/O=Evervault/OU=Cages/CN=Cage Test Intermediate CA" -addext "basicConstraints=critical,CA:TRUE" -key e2e-tests/sample-ca/sample-intermediate-key.pem -out e2e-tests/sample-ca/intermediate.csr
+openssl x509 -copy_extensions copyall -req -in e2e-tests/sample-ca/intermediate.csr -CA e2e-tests/sample-ca/sample-root-ca-cert.pem -CAkey e2e-tests/sample-ca/sample-root-ca-key.pem -out e2e-tests/sample-ca/sample-intermediate-cert.pem -days 365 -sha256
+rm e2e-tests/sample-ca/intermediate.csr
 
 # kill container if it is left running by hanging test, then generate local testing certs
 if [ "${CI:-unset}" = "unset" ];
@@ -26,7 +38,6 @@ else
   ACME_ACCOUNT_EC_KEY=`cat ./e2e-tests/acme-key/key.pem` && export ACME_ACCOUNT_EC_KEY
   ACME_ACCOUNT_HMAC_KEY="cGxhY2Vob2xkZXI=" && export ACME_ACCOUNT_HMAC_KEY
   ACME_ACCOUNT_HMAC_KEY_ID="placeholder_id" && export ACME_ACCOUNT_HMAC_KEY_ID
-
 fi
 
 # install the node modules for customer process and test script
@@ -87,7 +98,6 @@ npm run websocket-tests
 
 echo "Testing that Cage is serving trustable cert chain"
 echo "Q" | openssl s_client -verifyCAfile sample-ca/sample-root-ca-cert.pem -showcerts -connect 0.0.0.0:443 | grep "Verification: OK"
-
 
 echo "Tests complete"
 docker compose down
