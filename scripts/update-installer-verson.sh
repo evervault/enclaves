1487e98436a1fae9a622434220d3619b9a780300
Hannah Neary <hannah@evervault.com>
2023-10-26 13:43:45 +0100

    Publish installer with semver

---
+++ scripts/update-installer-verson.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e 
+
+if [ -z "$1" ]; then
+    echo "Installer version is null. Exiting..."
+    exit 1
+fi
+
+release_version="$1"
+installer_hash="$2"
+
+echo installer_hash $installer_hash
+
+major_version=$(echo "$release_version" | cut -d '.' -f 1)
+
+echo "Release major version: $major_version"
+
+version_json=$(curl -s "https://cage-build-assets.evervault.com/runtime/versions")
+echo "Version response: $version_json"
+
+if [ $? -eq 0 ]; then
+  version_json=$(echo "$version_json" | jq --arg major_version "$major_version" --arg installer_hash "$installer_hash" '.versions[$major_version].installer = $installer_hash')
+  echo "Updated versions: $version_json"
+  echo "$version_json" > ./scripts/versions
+else
+  echo "Couldn't get versions from S3 $version_json"
+fi
