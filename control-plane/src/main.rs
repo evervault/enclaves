f23f584cb27b2d986740c6ff2aa5cc5ac008b5da
Liam <liam@evervault.com>
2023-11-02 13:06:21 +0000

    chore: remove backwards compat for stripping proxy protocol

--- control-plane/src/main.rs
+++ control-plane/src/main.rs
@@ -3,12 +3,11 @@ use control_plane::dns::{ExternalAsyncDnsResolver, InternalAsyncDnsResolver};
 use control_plane::stats_client::StatsClient;
 use control_plane::stats_proxy::StatsProxy;
 use control_plane::{config_server, tls_proxy};
-use shared::server::error::ServerResult;
 use shared::{print_version, utils::pipe_streams, ENCLAVE_CONNECT_PORT};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::process::Command;
 use storage_client_interface::s3;
-use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
+use tokio::io::AsyncWriteExt;
 use tokio::time::{sleep, Duration};
 
 use tokio::net::TcpListener;
@@ -26,8 +25,6 @@ use control_plane::{
 const CONTROL_PLANE_PORT: u16 = 443;
 #[cfg(not(feature = "enclave"))]
 const CONTROL_PLANE_PORT: u16 = 3031;
-
-const PROXY_PROTOCOL_MIN_VERSION: semver::Version = semver::Version::new(0, 0, 30);
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -190,12 +187,7 @@ async fn tcp_server() -> Result<()> {
         }
     };
 
-    let parsed_data_plane_version = configuration::get_data_plane_version()
-        .ok()
-        .and_then(|raw_data_plane_version| semver::Version::parse(&raw_data_plane_version).ok());
-
     loop {
-        let data_plane_version = parsed_data_plane_version.clone();
         let (mut connection, client_socket_addr) = match tcp_listener.accept().await {
             Ok(conn) => conn,
             Err(e) => {
@@ -219,35 +211,11 @@ async fn tcp_server() -> Result<()> {
                     }
                 };
 
-            if should_remove_proxy_protocol(data_plane_version.as_ref()) {
-                if let Err(e) = strip_proxy_protocol_and_pipe(connection, enclave_stream).await {
-                    log::error!("An error occurred while piping the connection over vsock - {e:?}");
-                }
-            } else if let Err(e) = pipe_streams(connection, enclave_stream).await {
+            if let Err(e) = pipe_streams(connection, enclave_stream).await {
                 log::error!("An error occurred while piping the connection over vsock - {e:?}");
             }
         });
     }
-}
-
-fn should_remove_proxy_protocol(data_plane_version: Option<&semver::Version>) -> bool {
-    match data_plane_version {
-        Some(version) => version.cmp(&PROXY_PROTOCOL_MIN_VERSION) == std::cmp::Ordering::Less,
-        _ => false,
-    }
-}
-
-async fn strip_proxy_protocol_and_pipe<
-    T1: AsyncRead + AsyncWrite + std::marker::Unpin + std::marker::Sync,
-    T2: AsyncRead + AsyncWrite + std::marker::Unpin + std::marker::Sync,
->(
-    connection: T1,
-    enclave_stream: T2,
-) -> ServerResult<(u64, u64)> {
-    let parsed_connection =
-        shared::server::proxy_protocol::try_parse_proxy_protocol(connection).await?;
-    let pipe_result = pipe_streams(parsed_connection, enclave_stream).await?;
-    Ok(pipe_result)
 }
 
 // Listen for SIGTERM and deregister task before shutting down
@@ -310,68 +278,4 @@ fn listen_for_shutdown_signal() {
             }
         };
     });
-}
-
-#[cfg(test)]
-mod test {
-    use crate::PROXY_PROTOCOL_MIN_VERSION;
-
-    use super::{should_remove_proxy_protocol, strip_proxy_protocol_and_pipe};
-    use tokio_test::io::Builder;
-
-    fn build_proxy_protocol_header() -> Vec<u8> {
-        let header = ppp::v2::Builder::with_addresses(
-            ppp::v2::Version::Two | ppp::v2::Command::Proxy,
-            ppp::v2::Protocol::Stream,
-            (
-                "1.2.3.4:80"
-                    .parse::<std::net::SocketAddr>()
-                    .expect("Infallible - hardcoded"),
-                "5.6.7.8:443"
-                    .parse::<std::net::SocketAddr>()
-                    .expect("Infallible - hardcoded"),
-            ),
-        );
-        header.build().expect("Infallible - hardcoded")
-    }
-
-    #[tokio::test]
-    async fn test_proxy_protocol_backwards_compatibility() {
-        let buf = build_proxy_protocol_header();
-        let mut incoming_mock_builder = Builder::new();
-        incoming_mock_builder.read(&buf[..]);
-        let dummy_data = b"Hello, world".to_vec();
-        incoming_mock_builder.read(&dummy_data[..]);
-        let mock_incoming_proxied_conn = incoming_mock_builder.build();
-
-        let mut target_mock_builder = Builder::new();
-        // configure target stream to only expect the bytes following proxy protocol
-        target_mock_builder.write(&dummy_data[..]);
-        let mock_target_conn = target_mock_builder.build();
-        // test will fail if the target stream receives unexpected data
-        let pipe_result =
-            strip_proxy_protocol_and_pipe(mock_incoming_proxied_conn, mock_target_conn).await;
-        assert!(pipe_result.is_ok());
-    }
-
-    #[test]
-    fn test_should_remove_proxy_protocol_for_old_data_plane() {
-        let old_data_plane = Some(semver::Version::new(0, 0, 20));
-        let should_remove_pp = should_remove_proxy_protocol(old_data_plane.as_ref());
-        assert!(should_remove_pp);
-    }
-
-    #[test]
-    fn test_should_not_remove_proxy_protocol_for_new_data_plane() {
-        let new_data_plane = Some(semver::Version::new(0, 0, 35));
-        let should_remove_pp = should_remove_proxy_protocol(new_data_plane.as_ref());
-        assert_eq!(should_remove_pp, false);
-    }
-
-    #[test]
-    fn test_should_not_remove_proxy_protocol_for_exact_match_data_plane() {
-        let min_data_plane = Some(PROXY_PROTOCOL_MIN_VERSION.clone());
-        let should_remove_pp = should_remove_proxy_protocol(min_data_plane.as_ref());
-        assert_eq!(should_remove_pp, false);
-    }
 }
