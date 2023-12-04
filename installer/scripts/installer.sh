19fd600bc8bbc9bc76b3d1287a735b4705ab2c2d
Hannah Neary <92307259+hanneary@users.noreply.github.com>
2023-12-04 09:13:34 +0000

    Add iptables to installer (#168)

--- installer/scripts/installer.sh
+++ installer/scripts/installer.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-
 IFCONFIG_PATH=`command -v ifconfig`
 if [ -z "$IFCONFIG_PATH" ]; then
-  ### Install ifconfig
   echo "Installing prebuilt ifconfig"
   IFCONFIG_TARGET_PATH=/usr/local/bin/ifconfig
   install -m 0755 net-tools-2.10/ifconfig "$IFCONFIG_TARGET_PATH"
@@ -22,6 +20,18 @@ if [ -z "$RUNIT_PATH" ]; then
     exit 2
   fi
   echo "runit installed successfully"
+fi
+
+IPTABLES_PATH=`command -v iptables`
+if [ -z "$IPTABLES_PATH" ]; then
+  echo "Installing prebuilt iptables"
+  IPTABLES_TARGET_PATH=/usr/local/bin/iptables
+  mkdir -p /packages/iptables-1.8.10 
+  install -m 0755 ./iptables-1.8.10/iptables/xtables-legacy-multi "$IPTABLES_TARGET_PATH"
+  IPTABLES_PATH_POST_INSTALL=`command -v iptables`
+  echo "IPTABLES_PATH_POST_INSTALL: $IPTABLES_PATH_POST_INSTALL"
+  test "$IPTABLES_PATH_POST_INSTALL" = "$IPTABLES_TARGET_PATH" || exit 1
+  echo "iptables installed successfully"
 fi
 
 exit 0
