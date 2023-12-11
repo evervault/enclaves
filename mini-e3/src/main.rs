647c77bed4b58ffbf361854548979e51f2594e44
Eoin Boylan <jb.eoin@gmail.com>
2023-12-11 17:11:45 +0000

    Add hyper for http

--- mini-e3/src/main.rs
+++ mini-e3/src/main.rs
@@ -1,5 +1,14 @@
+use hyper::{Request, Response};
+use http_body_util::Full;
 use shared::server::{get_vsock_server, Listener};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use bytes::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::{service_fn};
+use hyper_util::rt::TokioIo;
+use serde::Deserialize;
+use std::convert::Infallible;
+use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
 
 fn main() {
     println!("Hello, world!");
@@ -13,36 +22,33 @@ fn main() {
 
 async fn start_server() {
     println!("Staring Server");
+
     let mut server = get_vsock_server(8001, shared::server::CID::Enclave)
         .await
         .unwrap();
-    loop {
-        let mut buffer = [0;1024];
-        match server.accept().await {
-            Ok(mut stream) => {
-                match stream.read(&mut buffer).await {
-                    Ok(size) => {
-                        let incoming_data = String::from_utf8_lossy(&buffer[..size]);
-                        println!("recived data {:?}", incoming_data);
-                    },
-                    Err(e) => {
-                        println!("failed to read from socket; err = {:?}", e);
-                    }
-                }
 
-                match stream.write(&buffer).await {
-                    Ok(size) => {
-                        println!("send data {:?}", size);
-                    },
-                    Err(e) => {
-                        println!("failed to write to socket; err = {:?}", e);
-                    }
-                }
-            },
-            Err(e) => {
-                println!("failed to accept client; error = {:?}", e);
-                continue;
+
+    loop {
+        let stream = server.accept().await.unwrap();
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        // let io = TokioIo::new(stream);
+        let io: TokioIo<tokio::net::TcpStream> = TokioIo::new(stream);
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
             }
-        };
+        });
     }
+}
+
+async fn hello(_: Request<impl hyper::body::Body>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
 }
