7a22c5c8ccf1d4c97c833b91b2a3915612786ad2
Liam <liam@evervault.com>
2024-05-13 12:16:08 +0100

    Update spoof ciphertext format to be matched by parser, add error handling

--- e2e-tests/mock-crypto/src/encrypt_mock.rs
+++ e2e-tests/mock-crypto/src/encrypt_mock.rs
@@ -1,10 +1,22 @@
 use std::fmt::Display;
+use thiserror::Error;
+use rand::RngCore;
+
+#[derive(Debug,Error)]
+pub enum MockCryptoError {
+  #[error(transparent)]
+  SerdeError(#[from] serde_json::Error),
+  #[error("Invalid cipher received")]
+  InvalidCipher
+}
 
 struct EncryptedValue(serde_json::Value);
 
 impl Display for EncryptedValue {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "ev:{}:{}:$", get_string_repr_for_serde_value(&self.0), convert_value_to_string(&self.0))
+    let val_str = convert_value_to_string(&self.0);
+    let encoded_val = base64::encode(&val_str);
+    write!(f, "ev:{}:{}:{}:{}:$", get_string_repr_for_serde_value(&self.0), mock_iv(), mock_pub_key(), encoded_val)
   }
 }
 
@@ -15,13 +27,15 @@ impl std::convert::Into<serde_json::Value> for EncryptedValue {
 }
 
 impl std::convert::TryFrom<String> for EncryptedValue {
-  type Error = serde_json::Error;
+  type Error = MockCryptoError;
 
   fn try_from(val: String) -> Result<Self, Self::Error> {
     if !val.starts_with("ev:") || !val.ends_with(":$") {
-      panic!("bad cipher received");
+      return Err(MockCryptoError::InvalidCipher);
     }
-    let parsed_val = serde_json::from_str(val.split(":").nth(2).unwrap())?;
+    let decoded_val = base64::decode(val.split(":").nth(4).unwrap()).map_err(|_| MockCryptoError::InvalidCipher)?;
+    let decoded_str = std::str::from_utf8(&decoded_val).map_err(|_| MockCryptoError::InvalidCipher)?;
+    let parsed_val = serde_json::from_str(decoded_str)?;
     Ok(Self(parsed_val))
   }
 }
@@ -40,7 +54,7 @@ pub fn encrypt(value: serde_json::Value) -> serde_json::Value {
   serde_json::Value::String(format!("{}", EncryptedValue(value)))
 }
 
-pub fn decrypt(value: String) -> Result<serde_json::Value, serde_json::Error> {
+pub fn decrypt(value: String) -> Result<serde_json::Value, MockCryptoError> {
   let enc_val = EncryptedValue::try_from(value)?;
   Ok(enc_val.0)
 }
@@ -49,4 +63,16 @@ pub fn convert_value_to_string(value: &serde_json::Value) -> String {
   value.as_str()
     .map(|val| val.to_string())
     .unwrap_or_else(|| serde_json::to_string(&value).unwrap())
+}
+
+fn mock_iv() -> String {
+  let mut iv = [0u8;16];
+  rand::thread_rng().fill_bytes(&mut iv);
+  base64::encode(iv)
+}
+
+fn mock_pub_key() -> String {
+  let mut pub_key = [0u8;44];
+  rand::thread_rng().fill_bytes(&mut pub_key);
+  base64::encode(pub_key)
 }
