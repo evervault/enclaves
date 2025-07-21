4eab01d9f5be6c31d04835af152645912f5fd5c6
Liam <liam@evervault.com>
2025-07-21 15:31:39 +0100

    chore: implement proper error mapping for sni parser

--- shared/src/server/sni.rs
+++ shared/src/server/sni.rs
@@ -6,19 +6,29 @@ use tls_parser::{
 #[derive(Debug, Error)]
 
 pub enum SNIError {
-    #[error("Couldn't parse hostname from request {0}")]
-    HostnameError(String),
+    #[error("Failed to parse hostname from packet")]
+    HostnameError,
     #[error("Attempted request to banned domain {0}")]
     EgressDomainNotAllowed(String),
     #[error("Client Hello not found")]
     ClientHelloMissing,
     #[error("TLS extension missing")]
     ExtensionMissing,
+    #[error("Insufficient data received to parse client hello")]
+    IncompleteHelloReceived,
+}
+
+impl<T> std::convert::From<tls_parser::nom::Err<T>> for SNIError {
+    fn from(value: tls_parser::nom::Err<T>) -> Self {
+        match value {
+            tls_parser::Err::Incomplete(_) => SNIError::IncompleteHelloReceived,
+            tls_parser::Err::Error(_) | tls_parser::Err::Failure(_) => SNIError::HostnameError,
+        }
+    }
 }
 
 pub fn get_hostname(data: &[u8]) -> Result<String, SNIError> {
-    let (_, parsed_request) = parse_tls_plaintext(data)
-        .map_err(|tls_parse_err| SNIError::HostnameError(format!("{tls_parse_err:?}")))?;
+    let (_, parsed_request) = parse_tls_plaintext(data).map_err(SNIError::from)?;
 
     let client_hello = match &parsed_request.msg[0] {
         TlsMessage::Handshake(TlsMessageHandshake::ClientHello(client_hello)) => client_hello,
@@ -29,10 +39,9 @@ pub fn get_hostname(data: &[u8]) -> Result<String, SNIError> {
         Some(raw_extensions) => raw_extensions,
         _ => return Err(SNIError::ExtensionMissing),
     };
-    
+
     let mut destination = "".to_string();
-    let (_, extensions) = parse_tls_extensions(raw_extensions)
-        .map_err(|tls_parse_err| SNIError::HostnameError(format!("{tls_parse_err:?}")))?;
+    let (_, extensions) = parse_tls_extensions(raw_extensions).map_err(SNIError::from)?;
 
     for extension in extensions {
         if let TlsExtension::SNI(sni_vec) = extension {
