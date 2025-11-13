c5830ca5ced0e549441750e2f5903f3bdc76d08e
Liam <liam@evervault.com>
2025-11-13 16:59:49 +0000

    chore: fmt

--- data-plane/src/health/agent.rs
+++ data-plane/src/health/agent.rs
@@ -11,15 +11,11 @@ use tokio::sync::oneshot::{
     channel as oneshot_channel, Receiver as OneshotReceiver, Sender as OneshotSender,
 };
 
+#[derive(Default)]
 enum HealthcheckAgentState {
+    #[default]
     Initializing,
     Ready,
-}
-
-impl std::default::Default for HealthcheckAgentState {
-    fn default() -> Self {
-        Self::Initializing
-    }
 }
 
 pub struct HealthcheckStatusRequest {
