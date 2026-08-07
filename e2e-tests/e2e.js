55d7e8f08e0c9c57a42a6b8da11c4ab58c3d4db5
Mario Savarese <mario.savarese@evervault.com>
2026-08-07 17:47:23 +0100

    test: add e2e tests to exercise malformed variable path

--- e2e-tests/e2e.js
+++ e2e-tests/e2e.js
@@ -107,6 +107,8 @@ describe("POST data to enclave", () => {
       headers: { "api-key": "placeholder" },
     });
     expect("123").to.deep.equal(result.data.ANOTHER_ENV_VAR);
+    expect("a b'c$d\"e`f").to.deep.equal(result.data.TRICKY_ENV_VAR);
+    expect(result.data.BAD_NAME).to.equal(null);
   });
 
   it("attestation doc", async () => {
--- e2e-tests/noTlsTests.js
+++ e2e-tests/noTlsTests.js
@@ -5,5 +5,7 @@ describe('GET env from enclave', () => {
     it('returns the injected environment var', async () => {
         const result =  await axios.get('http://enclave.localhost:443/env', { headers: { 'api-key': 'placeholder' } })
         expect("123").to.deep.equal(result.data.ANOTHER_ENV_VAR)
+        expect("a b'c$d\"e`f").to.deep.equal(result.data.TRICKY_ENV_VAR)
+        expect(result.data.BAD_NAME).to.equal(null)
     });
 });
